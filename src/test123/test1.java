9a70554f2da79a23152fd04fe3b6c8824d661296
tandhingra <tandhingra@deloitte.com>
2021-07-10 15:32:09 +0530

    second commit

--- src/test123/test1.java
+++ src/test123/test1.java
@@ -2,5 +2,19 @@ package test123;
 
 public class test1 {
 
-    
+    public static void main(String[] args) {
+
+        int[] arr = {100,200,300,400};
+
+        System.out.println("Array size:" + arr.length);
+
+        int[][] arr2;
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+
+
+    }
 }
